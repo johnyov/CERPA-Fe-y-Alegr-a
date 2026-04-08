f9ba5ac3e755d75d241ce4c152e265d513c6a15f
John <johnyov@gmail.com>
2026-04-07 20:42:32 -0400

    Solucion definitiva pantalla blanca usando punto barra

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import path from 'path';
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-  base: '/CERPA-Fe-y-Alegr-a/',
+  base: './',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
